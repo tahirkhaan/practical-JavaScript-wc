0dfa98b901da0842e07ad601dcb674575ad7aeac
tahirkhaan <tahirkhan.dream@gmail.com>
2017-07-26 14:31:46 +0500

    how to display and add array

---
+++ display_array.js
@@ -0,0 +1,5 @@
+var todos = ['item 1', 'item 2', 'item 3', 'item 4']
+console.log('todos') // it will display all items inside array
+
+todos.push('item 5') // it looks like
+['item 1', 'item 2', 'item 3', 'item 4', 'item 5']
---
+++ store.js
@@ -0,0 +1,3 @@
+['item 1', 'item 2', 'item 3'] // it is list but in programing its called array
+
+var todos = ['item 1', 'item 2', 'item 3', 'item 4'] // this is array in javascript
