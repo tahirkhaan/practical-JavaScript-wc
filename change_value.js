4ba44f336c05847ecf1cc48e7c2d9de4ab483d45
tahirkhaan <tahirkhan.dream@gmail.com>
2017-07-26 16:47:18 +0500

    change value in array

---
+++ change_value.js
@@ -0,0 +1,10 @@
+var todos = ['item1', 'item2', 'item3', 'item4']
+console.log(todos) //its will display array.
+['item1', 'item2', 'item3', 'item4']
+
+todos[0] //its display
+ "item1"
+todos[1] //its display
+ "item2"
+//for changing the value 
+todos[0] = 'new value' //its update the value of item1.
