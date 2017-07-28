605dbebc819f4054139ce766a0dfbe39e5f16eca
Tahir khan <tahirkhan.dream@gmail.com>
2017-07-28 16:14:53 +0500

    Update change_value.js

--- arrays/change_value.js
+++ arrays/change_value.js
@@ -1,7 +1,6 @@
 var todos = ['item1', 'item2', 'item3', 'item4']
 console.log(todos) //its will display array.
 ['item1', 'item2', 'item3', 'item4']
-
 todos[0] //its display
  "item1"
 todos[1] //its display
