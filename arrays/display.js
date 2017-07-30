eee0b4bd1035796ae10baa4ba7b5bae916f61917
Tahir khan <tahirkhan.dream@gmail.com>
2017-07-30 21:35:22 +0500

    code format

--- arrays/display.js
+++ arrays/display.js
@@ -1,7 +1,5 @@
 console.log('Hello there') // it will display Hello there.
 console.log('Hello there','Fahad')//it will display both.
-
 var todos = ['item1','item2','item3','item4','item5','item6']
-
 console.log(todos)//it will display array of name todos.
 console.log('My todos:',todos)//it will display string of my todos and then array of todos.
