1546ea64595f0d297a2025280f62b08c7d7f51fc
tahirkhaan <tahirkhan.dream@gmail.com>
2017-08-02 11:58:58 +0500

    display an array through function

---
+++ functions/display_function.js
@@ -0,0 +1,8 @@
+var todos = ['item 1', 'item 2', 'item 3'] // array
+//Now function start
+function displayTodos() {
+  console.log('My todos:', todos);
+}
+displayTodos()
+// the output of this function is
+My todos: ['item 1', 'item 2', 'item 3']
